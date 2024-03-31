e4748c893b4208d22fa8c2eaa25361a5da3b060c
feigi <f0527121376@gmail.com>
2024-03-31 21:24:51 +0300

    add a defualt get

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
-const express=require("express")
-const bodyParser=require("body-parser")
-const dotenv=require("dotenv")
-const cors=require("cors")
+const express = require("express");
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
+const cors = require("cors");
 
 const app = express();
 
-app.use(bodyParser.json())
-app.use(cors())
+app.use(bodyParser.json());
+app.use(cors());
 
-app.get("",(req,res)=>{
-    res.status(200).send("Hello world👍")
-})
+const sdk = require("api")("@render-api/v1.0#ja8rb1jlsxhh7qu");
 
-app.listen(3001, () => {
-    console.log(`my app is listening in http://localhost:3001`);
+sdk.auth("rnd_VgqLiNh4uWL6WMs1mFwGYNmbKwCS");
+
+app.get("", (req, res) => {
+  sdk
+    .getServices({ limit: "20" })
+    .then(({ data }) => res.status(200).send(data))
+    .catch((err) => res.status(200).send("Hello world👍",err));
 });
 
-const sdk = require('api')('@render-api/v1.0#ja8rb1jlsxhh7qu');
-
-sdk.auth('rnd_VgqLiNh4uWL6WMs1mFwGYNmbKwCS');
-sdk.getServices({limit: '20'})
-  .then(({ data }) => console.log(data))
-  .catch(err => console.error(err));
+app.listen(3001, () => {
+  console.log(`my app is listening in http://localhost:3001`);
+});
